9336e0b54a915900d7ff049633953811766983d9
olmosalexis <olmoslexis@gmail.com>
2024-07-29 01:11:22 -0500

    updating config and contact

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,8 +60,8 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        My inbox is open in case you have any questions or just wanna say a hello!
-        Let's get in touch if you have any relevant roles for me!
+      My inbox is always open. If you're curious about my work, questions about my 
+      experiences, or just in the mood for a friendly chat, don't hesitate to get in touch!
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
--- src/config.js
+++ src/config.js
@@ -1,26 +1,14 @@
 module.exports = {
-  email: 'chandrikadeb7@gmail.com',
+  email: 'olmoslexis@gmail.com',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/chandrikadeb7',
-    },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/chandrikadeb7',
-    },
-    {
-      name: 'Twitter',
-      url: 'https://twitter.com/chandrikadeb7',
+      url: 'https://github.com/olmoslexis',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/chandrika-deb',
-    },
-    {
-      name: 'Codepen',
-      url: 'https://codepen.io/chandrikadeb7',
+      url: 'https://www.linkedin.com/in/alexis-olmos/',
     },
   ],
 
